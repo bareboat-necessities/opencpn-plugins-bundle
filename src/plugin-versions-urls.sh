f5f7dcee392320e2b6eddcafb2be8ad2cefe3731
mgrouch <mgrushinskiy@gmail.com>
2021-09-04 00:27:56 -0400

    initial

--- src/plugin-versions-urls.sh
+++ src/plugin-versions-urls.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
+curl -s https://raw.githubusercontent.com/OpenCPN/plugins/master/ocpn-plugins.xml | \
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=10]" \
  -v "str:align(concat('\"', normalize-space(name), '\"'), '                                  ')" -o ' ' \
  -v "str:align(concat('\"', normalize-space(version), '\"'), '                               ')" -o ' ' \
  -v "concat('\"', normalize-space(tarball-url), '\"')" \
- -n  ~/.opencpn/ocpn-plugins.xml | sort
+ -n - | sort
--- src/plugin-versions.sh
+++ src/plugin-versions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
+curl -s https://raw.githubusercontent.com/OpenCPN/plugins/master/ocpn-plugins.xml | \
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=10]" \
  -v "str:align(concat('\"', normalize-space(name), '\"'), '                                  ')" -o ' ' \
  -v "str:align(concat('\"', normalize-space(version), '\"'), '                               ')" \
- -n  ~/.opencpn/ocpn-plugins.xml | sort
+ -n - | sort
