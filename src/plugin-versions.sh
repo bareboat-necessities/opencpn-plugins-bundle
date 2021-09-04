4b75f2ff5f81d4c7b78880e8abb3c88c0afc88e5
mgrouch <mgrushinskiy@gmail.com>
2021-09-04 00:00:11 -0400

    initial

--- src/plugin-versions.sh
+++ src/plugin-versions.sh
@@ -2,29 +2,5 @@
 
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=10]" \
- -v "normalize-space(version)" -o ' ' \
- -v "normalize-space(api-version)" -o ' ' \
- -v "normalize-space(target)" -o ' ' \
- -v "normalize-space(target-version)" -o ' ' \
- -v "normalize-space(name)" -o ' ' \
- -v "normalize-space(tarball-url)" \
- -n  ~/.opencpn/ocpn-plugins.xml | sort
-
-echo ==================================================
-
-xmlstarlet sel -t \
- -m "/plugins/plugin" \
- -v "normalize-space(version)" -o ' ' \
- -v "normalize-space(api-version)" -o ' ' \
- -v "normalize-space(target)" -o ' ' \
- -v "normalize-space(target-version)" -o ' ' \
- -v "normalize-space(name)" -o ' ' \
- -v "normalize-space(tarball-url)" \
- -n  ~/.opencpn/ocpn-plugins.xml | sort | grep armhf | grep -v stretch | grep rasp
-
-echo ==================================================
-
-xmlstarlet sel -t \
- -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=10]" \
- -v "normalize-space(name)" -o ' ' \
+ -o '"' -v "normalize-space(name)" -o '"' -o '                    ' -v "normalize-space(version)" \
  -n  ~/.opencpn/ocpn-plugins.xml | sort
