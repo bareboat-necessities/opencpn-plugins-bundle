904c2d0520cf4aaf858d3079819e8671bc775d9e
mgrouch <mgrushinskiy@gmail.com>
2021-09-04 00:40:06 -0400

    initial

---
+++ src/plugin-versions-urls-arm64.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+curl -s https://raw.githubusercontent.com/OpenCPN/plugins/master/ocpn-plugins.xml | \
+xmlstarlet sel -t \
+ -m "/plugins/plugin[normalize-space(target-arch)='arm64' and normalize-space(target)!='android-arm64']" \
+ -v "str:align(concat('\"', normalize-space(name), '\"'), '                                  ')" -o ' ' \
+ -v "str:align(concat('\"', normalize-space(version), '\"'), '                               ')" -o ' ' \
+ -v "concat('\"', normalize-space(tarball-url), '\"')" \
+ -n - | sort
