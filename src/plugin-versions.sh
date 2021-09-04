957508bd94f6a98ebdcc0bdc29a340e7b4c5cd96
mgrouch <mgrushinskiy@gmail.com>
2021-09-04 00:11:45 -0400

    initial

--- src/plugin-versions.sh
+++ src/plugin-versions.sh
@@ -2,6 +2,6 @@
 
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=10]" \
- -v "str:align(normalize-space(name), '                                         ')" -o ' ' \
+ -v "str:align(concat('\"', normalize-space(name), '\"'), '                                         ')" -o ' ' \
  -v "normalize-space(version)" \
  -n  ~/.opencpn/ocpn-plugins.xml | sort
