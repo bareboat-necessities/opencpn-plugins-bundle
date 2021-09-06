12cccf6b4a23871519e437af9ddc64da4afce0c2
mgrouch <mgrushinskiy@gmail.com>
2021-09-06 11:41:33 -0400

    initial

--- make-plugins-bundle.sh
+++ make-plugins-bundle.sh
@@ -6,9 +6,11 @@ if [ "$1" == "arm64" ]; then
   arch=arm64
 fi
 
-mkdir tmp-bundle-$arch || exit 1
+rm -rf tmp-bundle-$arch || exit 1
+mkdir tmp-bundle-$arch || exit 2
 cd tmp-bundle-$arch
 
 cat ../plugin-list-$arch-buster.txt | xargs -n 3 -P 8 bash -c 'wget -q $2'
+find . -name \*.tar.gz -print0 | xargs -0 -n 1 -P 8 bash -c 'echo $0'
 
 cd ..
