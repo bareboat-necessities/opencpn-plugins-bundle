6b0bac89028d4ca4c485e852619bc02bb6d8d35a
mgrouch <mgrushinskiy@gmail.com>
2021-09-06 15:31:32 -0400

    initial

--- make-plugins-bundle.sh
+++ make-plugins-bundle.sh
@@ -12,8 +12,14 @@ cd tmp-bundle-$arch
 
 xargs -n 3 -P 8 bash -c '../unpack-plugin.sh "$0" "$1" "$2"' < ../plugin-list-$arch-buster.txt
 
+mkdir bundle || exit 3
 for dir in download_dir/*/; do
-  echo $dir
+  cd "$dir"
+  files="$(ls -I metadata.xml -I *.tar.gz)"
+  for file in $files; do
+    cp -r -p $file ../../bundle/
+  done
+  cd ../..
 done
 
 cd ..
