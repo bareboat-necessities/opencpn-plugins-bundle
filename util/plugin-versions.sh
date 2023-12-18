c7e380b77a761a5fb7fa462fb6e90d88d8c70cbb
mgrouch <mgrushinskiy@gmail.com>
2023-12-17 22:43:50 -0500

    bookworm

--- util/plugin-versions.sh
+++ util/plugin-versions.sh
@@ -3,6 +3,6 @@
 curl -s https://raw.githubusercontent.com/OpenCPN/plugins/master/ocpn-plugins.xml | \
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='armhf' and normalize-space(target)!='android-armhf' and normalize-space(target-version)=12]" \
- -v "str:align(concat('\"', normalize-space(name), '\"'), '                                  ')" -o ' ' \
- -v "str:align(concat('\"', normalize-space(version), '\"'), '                               ')" \
+ -v "str:align(concat('\"', normalize-space(name), '\"'), '                                    ')" -o ' ' \
+ -v "str:align(concat('\"', normalize-space(version), '\"'), '                                 ')" \
  -n - | sort
