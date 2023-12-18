4d301b9bb46019d3fe2a0d97d5580507f5e30adf
mgrouch <mgrushinskiy@gmail.com>
2023-12-17 22:45:12 -0500

    bookworm

--- util/plugin-versions-urls-arm64.sh
+++ util/plugin-versions-urls-arm64.sh
@@ -3,7 +3,7 @@
 curl -s https://raw.githubusercontent.com/OpenCPN/plugins/master/ocpn-plugins.xml | \
 xmlstarlet sel -t \
  -m "/plugins/plugin[normalize-space(target-arch)='arm64' and normalize-space(target)!='android-arm64']" \
- -v "str:align(concat('\"', normalize-space(name), '\"'), '                                  ')" -o ' ' \
- -v "str:align(concat('\"', normalize-space(version), '\"'), '                               ')" -o ' ' \
+ -v "str:align(concat('\"', normalize-space(name), '\"'), '                                       ')" -o ' ' \
+ -v "str:align(concat('\"', normalize-space(version), '\"'), '                                    ')" -o ' ' \
  -v "concat('\"', normalize-space(tarball-url), '\"')" \
  -n - | grep -v bionic | grep -v buster | grep -v ubuntu | grep -v bullseye | grep -v arm64-11 | grep -v ebian-11 | grep -v wx32 | sort
