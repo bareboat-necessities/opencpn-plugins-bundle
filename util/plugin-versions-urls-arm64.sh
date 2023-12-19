f6e8de3826eff51be414f64bcb72d0007f600e04
mgrouch <mgrushinskiy@gmail.com>
2023-12-19 00:45:01 -0500

    bookworm

--- util/plugin-versions-urls-arm64.sh
+++ util/plugin-versions-urls-arm64.sh
@@ -6,4 +6,5 @@ xmlstarlet sel -t \
  -v "str:align(concat('\"', normalize-space(name), '\"'), '                                       ')" -o ' ' \
  -v "str:align(concat('\"', normalize-space(version), '\"'), '                                    ')" -o ' ' \
  -v "concat('\"', normalize-space(tarball-url), '\"')" \
- -n - | grep -v bionic | grep -v buster | grep -v ubuntu | grep -v bullseye | grep -v arm64-11 | grep -v ebian-11 | grep -v wx32 | sort
+ -n - | grep -v bionic | grep -v buster | grep -v ubuntu | grep -v bullseye | grep -v arm64-11 | \
+ grep -v ebian-11 | grep -v wx32 | grep -v "calculator-alpha" | sort
